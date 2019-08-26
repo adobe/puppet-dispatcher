8034bdb0624e55207211c905e7a2c76338172826
Bryan Stopp <bryan.stopp@gmail.com>
2019-08-25 22:38:51 -0500

    Adding all main configuration and module management

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,11 +4,69 @@
 #
 # @example
 #   include dispatcher
-class dispatcher {
+class dispatcher(
+  Stdlib::Filesource $module_file,
+  Boolean $decline_root,
+  Stdlib::Absolutepath $log_file,
+  Enum['error', 'warn', 'info', 'debug', 'trace'] $log_level,
+  Variant[Boolean, Pattern[/^[\d\-,]+$/]] $pass_error,
+  Boolean $use_processed_url,
+  Optional[Integer[0]] $keep_alive_timeout                    = undef,
+  Optional[Boolean] $no_cannon_url                            = undef
+) {
 
   # Check for Apache because it is used by parameter defaults
   if ! defined(Class['apache']) {
     fail('You must include the Apache class before using any dispatcher class or resources.')
   }
 
+  if $::osfamily == 'RedHat' or $::operatingsystem =~ /^[Aa]mazon$/ {
+    $_mod_path = "${::apache::httpd_dir}/${::apache::lib_path}"
+    $_farm_path = $::apache::mod_dir
+
+  } elsif $::osfamily == 'Debian' {
+    $_mod_path = $::apache::lib_path
+    $_farm_path = $::apache::mod_enable_dir
+  } else {
+    fail("Class['dispatcher']: Unsupported osfamily: ${::osfamily}")
+  }
+  $_filename = basename($module_file)
+
+  $_owner = 'root'
+  $_group = $::apache::params::root_group
+
+  apache::mod { 'dispatcher':
+    lib => 'mod_dispatcher.so',
+  }
+
+  file { "${_mod_path}/${_filename}" :
+    ensure => file,
+    owner  => $_owner,
+    group  => $_group,
+    source => $module_file,
+  }
+
+  file { "${_mod_path}/mod_dispatcher.so" :
+    ensure => link,
+    owner  => $_owner,
+    group  => $_group,
+    target => "${_mod_path}/${_filename}",
+    notify => Class['Apache::Service'],
+  }
+
+  file { "${_farm_path}/dispatcher.conf" :
+    ensure  => file,
+    owner   => $_owner,
+    group   => $_group,
+    content => template("${module_name}/dispatcher.conf.erb"),
+    notify  => Class['Apache::Service'],
+  }
+
+  file { "${_farm_path}/dispatcher.farms.any" :
+    ensure => file,
+    owner  => $_owner,
+    group  => $_group,
+    source => 'puppet:///modules/dispatcher/dispatcher.farms.any',
+    notify => Class['Apache::Service'],
+  }
 }
--- spec/classes/dispatcher_spec.rb
+++ spec/classes/dispatcher_spec.rb
@@ -1,22 +1,444 @@
 require 'spec_helper'
 
-describe 'dispatcher' do
+describe 'dispatcher', type: :class do
 
   let(:pre_condition) do
     'class { "apache" : }'
   end
+  let(:default_params) { { module_file: '/full/path/to/file-with-version.so' } }
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) { default_params }
 
-      it { is_expected.to compile }
+      case os_facts[:osfamily]
+      when 'RedHat'
+        let(:lib_path) do
+          "#{catalogue.resource('Class[apache]').parameters[:httpd_dir]}/#{catalogue.resource('Class[apache]').parameters[:lib_path]}"
+        end
+        let(:mod_dir) { catalogue.resource('Class[apache]').parameters[:mod_dir] }
+        let(:log_root) { catalogue.resource('Class[apache]').parameters[:logroot] }
+      when 'Debian'
+        let(:lib_path) { catalogue.resource('Class[apache]').parameters[:lib_path] }
+        let(:mod_dir) { catalogue.resource('Class[apache]').parameters[:mod_enable_dir] }
+        let(:log_root) { catalogue.resource('Class[apache]').parameters[:logroot] }
+      end
+
+      context 'requires Apache' do
+        let(:pre_condition) { '' }
+
+        it { is_expected.to raise_error(%r{You must include the Apache class}) }
+      end
+
+      context 'missing module file' do
+        let(:params) { default_params.delete('module_file') }
+
+        it { is_expected.to raise_error(%r{expects a value for parameter 'module_file'}) }
+      end
+
+      context 'with default parameters' do
+        it { is_expected.to compile }
+        it do
+          apache = catalogue.resource('Class[apache]')
+          is_expected.to contain_class('dispatcher').only_with(
+            name:               'Dispatcher',
+            module_file:        '/full/path/to/file-with-version.so',
+            decline_root:       true,
+            log_file:           "#{apache.parameters[:logroot]}/dispatcher.log",
+            log_level:          'warn',
+            pass_error:         false,
+            use_processed_url:  true,
+          )
+        end
+
+        it do
+          is_expected.to contain_file("#{lib_path}/file-with-version.so").with(
+            ensure: 'file',
+            owner: 'root',
+            group: 'root',
+            source: '/full/path/to/file-with-version.so',
+          )
+        end
+
+        it do
+          is_expected.to contain_apache__mod('dispatcher')
+        end
+
+        it do
+          is_expected.to contain_file("#{lib_path}/mod_dispatcher.so").with(
+            ensure: 'link',
+            owner:  'root',
+            group:  'root',
+            target: "#{lib_path}/file-with-version.so",
+          ).that_notifies('Class[Apache::Service]')
+        end
+
+        it do
+          is_expected.to contain_file("#{mod_dir}/dispatcher.conf").with(
+            ensure: 'file',
+            owner:  'root',
+            group:  'root',
+          ).with_content(
+            %r{.*DispatcherConfig\s+#{mod_dir}/dispatcher.farms.any},
+          ).with_content(
+            %r{.*DispatcherLog\s+#{log_root}/dispatcher.log},
+          ).with_content(
+            %r{.*DispatcherLogLevel\s+warn},
+          ).with_content(
+            %r{.*DispatcherDeclineRoot\s+On},
+          ).with_content(
+            %r{.*DispatcherUseProcessedURL\s+On},
+          ).with_content(
+            %r{.*DispatcherPassError\s+0},
+          ).without_content(
+            %r{.*DispatcherKeepAliveTimeout.*},
+          ).without_content(
+            %r{.*DispatcherNoCanonURL.*},
+          ).that_notifies('Class[Apache::Service]')
+        end
+
+        it do
+          is_expected.to contain_file("#{mod_dir}/dispatcher.farms.any").with(
+            ensure: 'file',
+            owner:  'root',
+            group:  'root',
+            source: 'puppet:///modules/dispatcher/dispatcher.farms.any',
+          ).that_notifies('Class[Apache::Service]')
+        end
+      end
+
+      context 'with custom parameters' do
+        let(:params) do
+          {
+            module_file: '/path/to/module/file.so',
+            decline_root: false,
+            log_file: '/custom/path/to/logfile.log',
+            log_level: 'debug',
+            pass_error: '400-411,413-417,500',
+            use_processed_url: false,
+            keep_alive_timeout: 0,
+            no_cannon_url: true,
+          }
+        end
+
+        it { is_expected.to compile }
+        it do
+          is_expected.to contain_class('dispatcher').only_with(
+            name:               'Dispatcher',
+            module_file:        '/path/to/module/file.so',
+            decline_root:       false,
+            log_file:           '/custom/path/to/logfile.log',
+            log_level:          'debug',
+            pass_error:         '400-411,413-417,500',
+            use_processed_url:  false,
+            keep_alive_timeout: 0,
+            no_cannon_url:      true,
+          )
+        end
+
+        it do
+          is_expected.to contain_file("#{lib_path}/file.so").with(
+            ensure: 'file',
+            owner: 'root',
+            group: 'root',
+            source: '/path/to/module/file.so',
+          )
+        end
+
+        it do
+          is_expected.to contain_apache__mod('dispatcher')
+        end
+
+        it do
+          is_expected.to contain_file("#{lib_path}/mod_dispatcher.so").with(
+            ensure: 'link',
+            owner:  'root',
+            group:  'root',
+            target: "#{lib_path}/file.so",
+          ).that_notifies('Class[Apache::Service]')
+        end
+
+        it do
+          is_expected.to contain_file("#{mod_dir}/dispatcher.conf").with(
+            ensure: 'file',
+            owner:  'root',
+            group:  'root',
+          ).with_content(
+            %r{.*DispatcherConfig\s+#{mod_dir}/dispatcher.farms.any},
+          ).with_content(
+            %r{.*DispatcherLog\s+\/custom\/path\/to\/logfile.log$},
+          ).with_content(
+            %r{.*DispatcherLogLevel\s+debug$},
+          ).with_content(
+            %r{.*DispatcherDeclineRoot\s+Off$},
+          ).with_content(
+            %r{.*DispatcherUseProcessedURL\s+Off$},
+          ).with_content(
+            %r{.*DispatcherPassError\s+400-411,413-417,500$},
+          ).with_content(
+            %r{.*DispatcherKeepAliveTimeout\s+0$},
+          ).with_content(
+            %r{.*DispatcherNoCanonURL\s+On$},
+          ).that_notifies('Class[Apache::Service]')
+        end
+
+        it do
+          is_expected.to contain_file("#{mod_dir}/dispatcher.farms.any").with(
+            ensure: 'file',
+            owner:  'root',
+            group:  'root',
+            source: 'puppet:///modules/dispatcher/dispatcher.farms.any',
+          ).that_notifies('Class[Apache::Service]')
+        end
+      end
     end
   end
 
-  context 'requires Apache' do
-    let(:pre_condition) { '' }
+  describe 'parameter validation' do
 
-    it { is_expected.to raise_error(%r{You must include the Apache class}) }
+    supported_os = [{ 'operatingsystem' => 'CentOS', 'operatingsystemrelease' => ['7'] }]
+    on_supported_os(supported_os: supported_os).each do |_os, os_facts|
+      let(:facts) { os_facts }
+      let(:params) { { module_file: '/full/path/to/file-with-version.so' } }
+      let(:lib_path) do
+        "#{catalogue.resource('Class[apache]').parameters[:httpd_dir]}/#{catalogue.resource('Class[apache]').parameters[:lib_path]}"
+      end
+
+      context 'module_file' do
+        context 'fully qualified file' do
+          it { is_expected.to compile }
+        end
+
+        context 'URL path' do
+          let(:params) { { module_file: 'https://full/path/to/file-with-version.so' } }
+
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file("#{lib_path}/file-with-version.so").with(
+              ensure: 'file',
+              owner: 'root',
+              group: 'root',
+              source: 'https://full/path/to/file-with-version.so',
+            )
+          end
+        end
+
+        context 'File reference' do
+          let(:params) { { module_file: 'file:///full/path/to/file-with-version.so' } }
+
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file("#{lib_path}/file-with-version.so").with(
+              ensure: 'file',
+              owner: 'root',
+              group: 'root',
+              source: 'file:///full/path/to/file-with-version.so',
+            )
+          end
+        end
+
+        context 'puppet file reference' do
+          let(:params) { { module_file: 'puppet:///full/path/to/file-with-version.so' } }
+
+          it { is_expected.to compile }
+          it do
+            is_expected.to contain_file("#{lib_path}/file-with-version.so").with(
+              ensure: 'file',
+              owner: 'root',
+              group: 'root',
+              source: 'puppet:///full/path/to/file-with-version.so',
+            )
+          end
+        end
+
+      end
+
+      context 'decline_root' do
+        context 'true' do
+          let(:params) { default_params.merge(decline_root: true) }
+
+          it { is_expected.to compile }
+        end
+        context 'false' do
+          let(:params) { default_params.merge(decline_root: false) }
+
+          it { is_expected.to compile }
+        end
+
+        context 'invalid' do
+          let(:params) { default_params.merge(decline_root: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'decline_root' expects a Boolean value}) }
+        end
+      end
+
+      context 'keep_alive_timeout' do
+        context 'positive number' do
+          let(:params) { default_params.merge(keep_alive_timeout: 120) }
+
+          it { is_expected.to compile }
+        end
+        context 'invalid number' do
+          let(:params) { default_params.merge(keep_alive_timeout: -60) }
+
+          it { is_expected.to raise_error(%r{parameter 'keep_alive_timeout' expects a value of type Undef or Integer}) }
+        end
+
+        context 'NaN' do
+          let(:params) { default_params.merge(keep_alive_timeout: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'keep_alive_timeout' expects a value of type Undef or Integer}) }
+        end
+      end
+
+      context 'log_level' do
+        context 'error' do
+          let(:params) { default_params.merge(log_level: 'error') }
+
+          it { is_expected.to compile }
+        end
+        context 'warn' do
+          let(:params) { default_params.merge(log_level: 'warn') }
+
+          it { is_expected.to compile }
+        end
+        context 'info' do
+          let(:params) { default_params.merge(log_level: 'info') }
+
+          it { is_expected.to compile }
+        end
+        context 'debug' do
+          let(:params) { default_params.merge(log_level: 'debug') }
+
+          it { is_expected.to compile }
+        end
+        context 'trace' do
+          let(:params) { default_params.merge(log_level: 'trace') }
+
+          it { is_expected.to compile }
+        end
+        context 'invalid' do
+          let(:params) { default_params.merge(log_level: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'log_level' expects a match for Enum\['debug', 'error', 'info', 'trace', 'warn'\]}) }
+        end
+      end
+
+      context 'log_file' do
+        context 'fully qualified path' do
+          let(:params) { default_params.merge(log_file: '/full/path/to/file.log') }
+
+          it { is_expected.to compile }
+        end
+        context 'invalid: relative path' do
+          let(:params) { default_params.merge(log_file: '../path/to/file.log') }
+
+          it { is_expected.to raise_error(%r{parameter 'log_file' expects a Stdlib::Absolutepath}) }
+        end
+
+        context 'invalid: no path' do
+          let(:params) { default_params.merge(log_file: 'file.log') }
+
+          it { is_expected.to raise_error(%r{parameter 'log_file' expects a Stdlib::Absolutepath}) }
+        end
+      end
+
+      context 'pass_error' do
+        context 'false' do
+          let(:params) { default_params.merge(pass_error: true) }
+
+          it { is_expected.to compile }
+        end
+        context 'true' do
+          let(:params) { default_params.merge(pass_error: false) }
+
+          it { is_expected.to compile }
+        end
+        context 'single response code' do
+          let(:params) { default_params.merge(pass_error: '500') }
+
+          it { is_expected.to compile }
+        end
+        context 'multiple response codes' do
+          let(:params) { default_params.merge(pass_error: '404,500') }
+
+          it { is_expected.to compile }
+        end
+        context 'response codes ranges' do
+          let(:params) { default_params.merge(pass_error: '400-411,413-417') }
+
+          it { is_expected.to compile }
+        end
+        context 'response codes ranges with single code' do
+          let(:params) { default_params.merge(pass_error: '400-411,413-417,500') }
+
+          it { is_expected.to compile }
+        end
+
+        context 'invalid' do
+          let(:params) { default_params.merge(pass_error: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'pass_error' expects a value of type Boolean or Pattern}) }
+        end
+      end
+
+      context 'use_processed_url' do
+        context 'true' do
+          let(:params) { default_params.merge(use_processed_url: true) }
+
+          it { is_expected.to compile }
+        end
+        context 'false' do
+          let(:params) { default_params.merge(use_processed_url: false) }
+
+          it { is_expected.to compile }
+        end
+
+        context 'invalid' do
+          let(:params) { default_params.merge(use_processed_url: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'use_processed_url' expects a Boolean value}) }
+        end
+      end
+
+      context 'keep_alive_timeout' do
+        context 'positive number' do
+          let(:params) { default_params.merge(keep_alive_timeout: 120) }
+
+          it { is_expected.to compile }
+        end
+        context 'invalid number' do
+          let(:params) { default_params.merge(keep_alive_timeout: -60) }
+
+          it { is_expected.to raise_error(%r{parameter 'keep_alive_timeout' expects a value of type Undef or Integer}) }
+        end
+
+        context 'invalid' do
+          let(:params) { default_params.merge(keep_alive_timeout: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'keep_alive_timeout' expects a value of type Undef or Integer}) }
+        end
+      end
+
+      context 'no_cannon_url' do
+        context 'true' do
+          let(:params) { default_params.merge(no_cannon_url: true) }
+
+          it { is_expected.to compile }
+        end
+        context 'false' do
+          let(:params) { default_params.merge(no_cannon_url: false) }
+
+          it { is_expected.to compile }
+        end
+
+        context 'invalid' do
+          let(:params) { default_params.merge(no_cannon_url: 'invalid') }
+
+          it { is_expected.to raise_error(%r{parameter 'no_cannon_url' expects a value of type Undef or Boolean}) }
+        end
+      end
+    end
   end
 end
